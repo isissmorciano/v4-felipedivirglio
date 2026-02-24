3dc4415db1413a6996107e5d4b378be5d54d92e3
Felipe Di Virgilio <felipemichele.divirgilio@studenti.isissgobetti.it>
2026-02-24 11:58:11 +0100

    sto lavorando sul main

--- divirgilio.py
+++ divirgilio.py
@@ -13,8 +13,19 @@ def carica_biblioteca(nome_file: str) -> list[dict]:
         with open(nome_file, "r", encoding="utf-8") as file:
             libri = json.load(file)
         return libri
-    except FileNotFoundError
+    except FileNotFoundError:
+        print(f"Errore il file '{nome_file}' non è stato trovato")
+        return []
+    except json.JSONDecodeError as e:
+        print(f"Errore nel persing JSON: {e}")
+        return []
 
+def filtra_per_genere(libri: list[dict], genere: str) -> list[dict]:
+    filtrati = []
+    for libro in libri:
+        if libro ["genere"] == genere:
+            filtrati.append(libro)
+    return filtrati
 
 
 
@@ -47,3 +58,14 @@ def main() -> None:
     {"titolo": "Dune", "genere": "Fantascienza", "anno": 1965},
     {"titolo": "Harry Potter", "genere": "Fantasy", "anno": 1997}
     ]
+
+    nome_file = "biblioteca.json"
+    salva_biblioteca(libri, nome_file)
+    libri_caricati = carica_biblioteca(nome_file)
+    filtra = filtra_per_genere(libri_caricati, genere: str)
+    print(f"Libri di {filtra}")
+    print(libri('anno'))
+    print(libri('titolo'))
+    
+
+main()
